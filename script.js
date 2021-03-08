2a7567e9d7f5bae500a9b5846487ccd6b923436f
Dinesh Chaudhari <dinesh.chaudhari0310@gmail.com>
2021-03-08 16:08:27 +0530

    fix navbar for mobile size

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 window.onload = function () {
     // or enable responsive functionality:
     GitHubCalendar(".calendar", "dinesh-0310", { responsive: true });
+    const button = document.getElementById("btn");
+    const nav_content = document.querySelector('.nav_content')
+    button.addEventListener('click',()=>{
+      nav_content.classList.toggle("show")
+    })
+    const nav_list = document.querySelectorAll('.nav_list');
 
-   
+    function linkAction(){
+      nav_list.forEach(ele =>{
+        ele.classList.remove("active")
+      })
+      this.classList.add('active')
+
+      //remove menu
+      nav_content.classList.remove("show")
+    }
+    nav_list.forEach(ele =>{
+      ele.addEventListener('click',linkAction)
+    })
     
 }
